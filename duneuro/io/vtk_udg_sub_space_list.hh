2402f682477a6cccd86eecb409536a903f11ed5a
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2016-04-28 10:42:32 +0200

    [UDG] add sub space list for using multidomain vtk functions

---
+++ duneuro/io/vtk_udg_sub_space_list.hh
@@ -0,0 +1,61 @@
+#ifndef DUNEURO_VTK_UDG_SUB_SPACE_LIST_HH
+#define DUNEURO_VTK_UDG_SUB_SPACE_LIST_HH
+
+#include <dune/pdelab/gridfunctionspace/subspace.hh>
+
+#include <dune/udg/io/vtkfunction.hh>
+
+namespace duneuro
+{
+  // simple TMP to construct a multidomainvtkgridfunction
+  // it iterates over the subspaces of a powergridfunctionspace,
+  // creates an unfittedvtkgridfunction for the associated domain,
+  // adds it to the multidmoainvtkgridfunction and performs the
+  // recursive call.
+  template <class PGFS, int n, int i = 0>
+  struct SubSpaceList : public SubSpaceList<PGFS, n, i + 1> {
+    typedef SubSpaceList<PGFS, n, i + 1> BaseT;
+    typedef Dune::PDELab::GridFunctionSubSpace<PGFS, Dune::TypeTree::TreePath<i>> SUB;
+    explicit SubSpaceList(const PGFS& gfs) : BaseT(gfs), sub(gfs)
+    {
+    }
+    template <class F, class U, class ST>
+    void add(F& func, const U& u, const ST& st) const
+    {
+      typedef Dune::UDG::UnfittedVTKGridFunction<SUB, U, ST> GF;
+      std::stringstream ss;
+      ss << func.name() << i;
+      func.add(new GF(ss.str(), sub, u, st, i));
+      BaseT::add(func, u, st);
+    }
+    template <class F, class U, class ST>
+    void addGradient(F& func, const U& u, const ST& st) const
+    {
+      typedef Dune::UDG::UnfittedVTKGridFunctionGradient<SUB, U, ST> GF;
+      std::stringstream ss;
+      ss << func.name() << i;
+      func.add(new GF(ss.str(), sub, u, st, i));
+      BaseT::addGradient(func, u, st);
+    }
+    SUB sub;
+  };
+  // base of recursion
+  template <class PGFS, int n>
+  struct SubSpaceList<PGFS, n, n> {
+    explicit SubSpaceList(const PGFS& gfs)
+    {
+    }
+    template <class F, class U, class ST>
+    void add(F& func, const U& u, const ST& st) const
+    {
+      // nothing to do here
+    }
+    template <class F, class U, class ST>
+    void addGradient(F& func, const U& u, const ST& st) const
+    {
+      // nothing to do here
+    }
+  };
+}
+
+#endif // DUNEURO_VTK_UDG_SUB_SPACE_LIST_HH
