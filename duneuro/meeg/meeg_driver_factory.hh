5c8b20a845e1ea759dd84a3b5c7656ab72ab1f0f
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2016-07-01 08:58:14 +0200

    [doc] make_meeg_driver

--- duneuro/meeg/meeg_driver_factory.hh
+++ duneuro/meeg/meeg_driver_factory.hh
@@ -13,6 +13,27 @@ namespace duneuro
   class MEEGDriverFactory
   {
   public:
+    /**
+     * \brief create a new meeg driver
+     *
+     * The type of the driver is given by the `type` parameter in the \p config ParameterTree.
+     * Currently supported
+     * are `fitted` and `udg`.
+     *
+     * - `fitted`: The fitted driver uses a mesh to describe the geometry. It takes two main
+     * parameters to select the appropriate driver.
+     *    - `solver_type`: defines the type of the fitted solver. Currently supported are `cg` and
+     * `dg`.
+     *    - `element_type` : defines the type of the mesh element. Currently supported are
+     * `tetrahedron` and `hexahedron`. For hexahedral meshes, geometry adaption can be activated by
+     * setting the `geometry_adapted` option to `true`. Note that `dune-subgrid` has to be available
+     * when using geometry adapted meshes.
+     * - `udg` : The udg driver uses a structured mesh and level set functions to describe the
+     * geometry. The number of compartments is set using the `compartments` parameter. Currently, 4
+     * and 5 compartments are supported.
+     *
+     * The configuration is passed on to the selected driver.
+     */
     static std::unique_ptr<MEEGDriverInterface> make_meeg_driver(const Dune::ParameterTree& config,
                                                                  DataTree dataTree = DataTree());
   };
