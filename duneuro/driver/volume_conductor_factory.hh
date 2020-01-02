10f00c17a0b2287de84a823ce990dd47430d56e7
Sophie Schrader <sophie.schrader@wwu.de>
2020-01-02 14:02:17 +0100

    documentation adapted

--- duneuro/driver/volume_conductor_factory.hh
+++ duneuro/driver/volume_conductor_factory.hh
@@ -19,10 +19,10 @@ public:
   /**
    * \brief create a new volume conductor
    *
-   * The type of the driver is given by the `type` parameter in the \p config
+   * The type of the volume conductor is given by the `type` parameter in the \p config
    * ParameterTree. Currently supported are `fitted` and `udg`.
    *
-   * - `fitted`: The fitted driver uses a mesh to describe the geometry. It
+   * - `fitted`: The fitted volume conductor uses a mesh to describe the geometry. It
    * takes two main parameters to select the appropriate driver.
    *    - `solver_type`: defines the type of the fitted solver. Currently
    * supported are `cg` and `dg`.
@@ -31,11 +31,11 @@ public:
    * geometry adaption can be activated by setting the `geometry_adapted` option
    * to `true`. Note that `dune-subgrid` has to be available when using geometry
    * adapted meshes.
-   * - `udg` : The udg driver uses a structured mesh and level set functions to
+   * - `udg` : The udg volume conductor uses a structured mesh and level set functions to
    * describe the geometry. The number of compartments is set using the
    * `compartments` parameter. Currently, 4 and 5 compartments are supported.
    *
-   * The configuration is passed on to the selected driver.
+   * The configuration is passed on to the selected volume conductor.
    */
   static std::shared_ptr<VolumeConductorInterface<dim>>
   make_volume_conductor(const Dune::ParameterTree &config,
