711b8bde9381c673271a4f7dc6130c447037dc30
Christian Engwer <christi@dune-project.org>
2020-06-09 16:30:54 +0200

    [cmake,tbb] guard HAVE_TBB definition
    since TBB is directly supported by newer dune-common versions, we have to
    guard the HAVE_TBB definition to avoid 'already defined' warnings.

--- config.h.cmake
+++ config.h.cmake
@@ -40,10 +40,12 @@
 /* Define to the revision of duneuro */
 #define DUNEURO_VERSION_REVISION @DUNEURO_VERSION_REVISION@
 
-#cmakedefine HAVE_TBB ENABLE_TBB
-#cmakedefine HAVE_HDF5WRAP ENABLE_HDF5WRAP
-#cmakedefine HAVE_HDF5 ENABLE_HDF5
-#cmakedefine HAVE_NIFTI ENABLE_NIFTI
+#ifndef HAVE_TBB
+#define HAVE_TBB ENABLE_TBB
+#endif
+#define HAVE_HDF5WRAP ENABLE_HDF5WRAP
+#define HAVE_HDF5 ENABLE_HDF5
+#define HAVE_NIFTI ENABLE_NIFTI
 
 /* end duneuro
    Everything below here will be overwritten
