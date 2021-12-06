190eb91cc6ef7a7894c2cdf0a1d788d717a1a72d
Malte <m_hoel20@uni-muenster.de>
2021-12-06 11:27:57 +0100

    enable localized cg source model to be used with the transfer matrix approach

--- duneuro/eeg/cg_source_model_factory.hh
+++ duneuro/eeg/cg_source_model_factory.hh
@@ -129,6 +129,17 @@ namespace duneuro
                                                    Vector>>(solver.volumeConductor(),
                                                             solver.functionSpace().getGFS(),
                                                             solver.elementSearch(), config);
+      } else if (type == "localized_subtraction") {
+        return std::make_shared<LocalizedSubtractionSourceModel<
+          typename Solver::Traits::VolumeConductor,
+          typename Solver::Traits::FunctionSpace,
+          Vector,
+          ContinuityType::continuous>>(
+            solver.volumeConductor(),
+            Dune::stackobject_to_shared_ptr(solver.functionSpace()),
+            solver.elementSearch(),
+            config,
+            solverConfig);
       } else {
         DUNE_THROW(duneuro::SourceModelException, "unknown source model \"" << FeatureManager::strip_prefix(type) << "\"");
       }
