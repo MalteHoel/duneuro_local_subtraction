06eba63b5b2219760dd7a6a021e9bb73d3d5eadf
Malte <m_hoel20@uni-muenster.de>
2024-02-24 17:32:16 +0100

    first implementation of dipole scan

--- duneuro/inverse/inverse_solver.hh
+++ duneuro/inverse/inverse_solver.hh
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <string>
+#include <cctype>
 
 #include <Eigen/Dense>
 #include <Eigen/SVD>
@@ -15,6 +17,8 @@ namespace duneuro {
 template <int dim, class Scalar>
 class InverseSolver {
 public:
+  enum class Beamformer {UG, AG, UNG, NAI, SAM};
+
   explicit InverseSolver(const Dune::ParameterTree& config)
     : config_(config)
     , leadfieldBound_(false)
@@ -22,6 +26,10 @@ public:
     , nrSourcePositions_(0)
     , stride_(0)
     , nrChannels_(0)
+    , signalCovariance_()
+    , noiseCovariance_()
+    , signalCovarianceBound_(false)
+    , noiseCovarianceBound_(false)
   {
   }
   
@@ -38,7 +46,39 @@ public:
     nrChannels_ = leadfield_.rows();
   }
   
-  std::tuple<std::vector<Scalar>, std::vector<Eigen::Vector3d>> dipoleScan(const Eigen::VectorXd& topography, const Dune::ParameterTree& dipoleScanConfig)
+  void bindSignalCovariance(const Eigen::MatrixXd& signalCovariance)
+  {
+    if (signalCovariance.rows() != signalCovariance.cols()) {
+      DUNE_THROW(Dune::Exception, "covariance matrix needs to be square");
+    }
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "bind leadfield before binding covariance matrix");
+    }
+    if(nrChannels_ != signalCovariance.rows()) {
+      DUNE_THROW(Dune::Exception, "dimensions of covariance do not match number of channels in leadfield");
+    }
+    
+    signalCovariance_ = signalCovariance;
+    signalCovarianceBound_ = true;
+  }
+  
+  void bindNoiseCovariance(const Eigen::MatrixXd& noiseCovariance)
+  {
+    if (noiseCovariance.rows() != noiseCovariance.cols()) {
+      DUNE_THROW(Dune::Exception, "covariance matrix needs to be square");
+    }
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "bind leadfield before binding covariance matrix");
+    }
+    if(nrChannels_ != noiseCovariance.rows()) {
+      DUNE_THROW(Dune::Exception, "dimensions of covariance do not match number of channels in leadfield");
+    }
+    
+    noiseCovariance_ = noiseCovariance;
+    noiseCovarianceBound_ = true;
+  }
+  
+  std::tuple<std::vector<Scalar>, std::vector<Eigen::Vector3d>> dipoleScan(const Eigen::VectorXd& topography, const Dune::ParameterTree& dipoleScanConfig) const
   {
     Scalar regularizationParameter = dipoleScanConfig.get<Scalar>("regularization_parameter");
     bool truncateLeadfield = dipoleScanConfig.get<bool>("truncate_leadfield");
@@ -48,7 +88,7 @@ public:
     
     // iterate over source space
     for(size_t i = 0; i < nrSourcePositions_; ++i) {
-      Eigen::MatrixXd currentLeadField = leadfield_.block(0, stride_ * i, nrChannels_, 3);
+      Eigen::MatrixXd currentLeadField = leadfield_.block(0, stride_ * i, nrChannels_, stride_);
       Eigen::JacobiSVD<Eigen::MatrixXd> leadfield_svd;
       leadfield_svd.compute(currentLeadField, Eigen::ComputeThinU | Eigen::ComputeThinV);
       
@@ -61,13 +101,13 @@ public:
       }
       
       if(truncateLeadfield) {
-        inverseS[dim - 1] = 0;
+        inverseS[dim - 1] = 0.0;
       }
       else {
         inverseS[dim - 1] = singularValues[i] / (singularValues[i] * singularValues[i] + regularizationParameter);
       }
       
-      Eigen::Vector3d reconstruction = leadfield_svd.matrixV() * inverseS.asDiagonal() * leadfield_svd.matrixU().transpose();
+      Eigen::Vector3d reconstruction = leadfield_svd.matrixV() * inverseS.asDiagonal() * leadfield_svd.matrixU().transpose() * topography;
       Scalar residual_variance = (topography - currentLeadField * reconstruction).squaredNorm();
       
       reconstructedOrientations[i] = reconstruction;
@@ -75,6 +115,11 @@ public:
     }
     
     return {goodnessOfFit, reconstructedOrientations};
+  }
+  
+  std::vector<Eigen::Vector3d> estimateOrientationsScalarBeamforming(const Dune::ParameterTree& beamformerConfig) const
+  {
+    Beamformer beamformer = beamformerFromString(beamformerConfig.get<std::string>("beamformer"));
   }
   
   std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> getSVD(const Eigen::MatrixXd& matrix)
@@ -85,12 +130,39 @@ public:
   }
   
 private:
+
+  Beamformer beamformerFromString(const std::string& value)
+  {
+    // make string uppercase to cover different spellings
+    std::string valueLocal(value);
+    for(auto& character : valueLocal) {
+      character = std::toupper(character);
+    }
+    
+    if(value == "UG")
+      return Beamformer::UG;
+    else if (value == "AG")
+      return Beamformer::AG;
+    else if (value == "UNG")
+      return Beamformer::UNG;
+    else if (value == "NAI")
+      return Beamformer::NAI;
+    else if (value == "SAM")
+      return Beamformer::SAM;
+    else
+      DUNE_THROW(Dune::Exception, "unknown beamformer, please choose one from {UG, AG, UNG, NAI, SAM}");
+  }
+
   Dune::ParameterTree config_;
   bool leadfieldBound_;
   Eigen::MatrixXd leadfield_;
   size_t nrSourcePositions_;
   size_t stride_;
   size_t nrChannels_;
+  Eigen::MatrixXd signalCovariance_;
+  Eigen::MatrixXd noiseCovariance_;
+  bool signalCovarianceBound_;
+  bool noiseCovarianceBound_;
 }; // InverseSolver class
 
 } // namespace duneuro
