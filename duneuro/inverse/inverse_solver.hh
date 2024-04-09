467c6ac18521cec4cc76448873c7f0c381c81fc1
Malte <m_hoel20@uni-muenster.de>
2024-04-09 11:29:38 +0200

    add beamformers and sLORETA

--- duneuro/inverse/inverse_solver.hh
+++ duneuro/inverse/inverse_solver.hh
@@ -6,9 +6,12 @@
 #include <tuple>
 #include <string>
 #include <cctype>
+#include <limits>
 
 #include <Eigen/Dense>
 #include <Eigen/SVD>
+#include <Eigen/QR>
+#include <Eigen/Eigenvalues>
 
 #include <dune/common/parametertree.hh>
 
@@ -18,6 +21,10 @@ template <int dim, class Scalar>
 class InverseSolver {
 public:
   enum class Beamformer {UG, AG, UNG, NAI, SAM};
+  using SourceVector = Eigen::Matrix<Scalar, dim, 1>;
+  using SourceMatrix = Eigen::Matrix<Scalar, dim, dim>;
+  using ScanResult = std::tuple<std::vector<Scalar>, std::vector<SourceVector>>;
+  using LeadfieldMatrix = Eigen::Matrix<Scalar, Eigen::Dynamic, dim>;
 
   explicit InverseSolver(const Dune::ParameterTree& config)
     : config_(config)
@@ -78,23 +85,33 @@ public:
     noiseCovarianceBound_ = true;
   }
   
-  std::tuple<std::vector<Scalar>, std::vector<Eigen::Vector3d>> dipoleScan(const Eigen::VectorXd& topography, const Dune::ParameterTree& dipoleScanConfig) const
+  // perform a dipole scan over the source space. The 3d variant also estimates the source
+  // orientation.
+  ScanResult dipoleScan3d(const Eigen::VectorXd& topography, const Dune::ParameterTree& dipoleScanConfig) const
   {
+    // validate state
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "leadfield needs to be specified before dipole scanning can be performed");
+    }
+    if(stride_ != dim) {
+      DUNE_THROW(Dune::Exception, "stride of leadfield does not match dimension, please use another reconstruction approach");
+    }
+    
     Scalar regularizationParameter = dipoleScanConfig.get<Scalar>("regularization_parameter");
     bool truncateLeadfield = dipoleScanConfig.get<bool>("truncate_leadfield");
     
     std::vector<Scalar> goodnessOfFit(nrSourcePositions_);
-    std::vector<Eigen::Vector3d> reconstructedOrientations(nrSourcePositions_);
+    std::vector<SourceVector> reconstructedOrientations(nrSourcePositions_);
     
     // iterate over source space
     for(size_t i = 0; i < nrSourcePositions_; ++i) {
-      Eigen::MatrixXd currentLeadField = leadfield_.block(0, stride_ * i, nrChannels_, stride_);
-      Eigen::JacobiSVD<Eigen::MatrixXd> leadfield_svd;
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      Eigen::JacobiSVD<LeadfieldMatrix> leadfield_svd;
       leadfield_svd.compute(currentLeadField, Eigen::ComputeThinU | Eigen::ComputeThinV);
       
       // compute regularized inverse
-      Eigen::Vector3d singularValues = leadfield_svd.singularValues();
-      Eigen::Vector3d inverseS;
+      SourceVector singularValues = leadfield_svd.singularValues();
+      SourceVector inverseS;
       
       for(size_t i = 0; i < dim - 1; ++i) {
         inverseS[i] = singularValues[i] / (singularValues[i] * singularValues[i] + regularizationParameter); 
@@ -107,7 +124,7 @@ public:
         inverseS[dim - 1] = singularValues[i] / (singularValues[i] * singularValues[i] + regularizationParameter);
       }
       
-      Eigen::Vector3d reconstruction = leadfield_svd.matrixV() * inverseS.asDiagonal() * leadfield_svd.matrixU().transpose() * topography;
+      SourceVector reconstruction = leadfield_svd.matrixV() * inverseS.asDiagonal() * leadfield_svd.matrixU().transpose() * topography;
       Scalar residual_variance = (topography - currentLeadField * reconstruction).squaredNorm();
       
       reconstructedOrientations[i] = reconstruction;
@@ -117,21 +134,208 @@ public:
     return {goodnessOfFit, reconstructedOrientations};
   }
   
-  std::vector<Eigen::Vector3d> estimateOrientationsScalarBeamforming(const Dune::ParameterTree& beamformerConfig) const
+  ScanResult scalarBeamforming3d(const Dune::ParameterTree& beamformerConfig) const
   {
     Beamformer beamformer = beamformerFromString(beamformerConfig.get<std::string>("beamformer"));
+    Scalar regularizationParameter = beamformerConfig.get<Scalar>("regularization_parameter");
+    
+    // validate state
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "no leadfield specified");
+    }
+    if(!signalCovarianceBound_) {
+      DUNE_THROW(Dune::Exception, "no signal covariance matrix specified");
+    }
+    if((beamformer == Beamformer::NAI || beamformer == Beamformer::SAM) && !noiseCovarianceBound_) {
+      DUNE_THROW(Dune::Exception, "NAI and SAM beamformers require a signal covariance matrix");
+    }
+    
+    // perform beamformer scan
+    std::vector<Scalar> beamformerMeasure(nrSourcePositions_);
+    std::vector<SourceVector> estimatedOrientations(nrSourcePositions_);
+    
+    if(beamformer == Beamformer::UG) 
+      scalarUGBeamformer3d(beamformerMeasure, estimatedOrientations, regularizationParameter);
+    else if(beamformer == Beamformer::AG)
+      scalarAGBeamformer3d(beamformerMeasure, estimatedOrientations, regularizationParameter);
+    else if(beamformer == Beamformer::UNG)
+      scalarUNGBeamformer3d(beamformerMeasure, estimatedOrientations, regularizationParameter);
+    else if(beamformer == Beamformer::NAI) {
+      Scalar regularizationParameterNoise = beamformerConfig.get<Scalar>("regularization_parameter_noise");
+      scalarNAIBeamformer3d(beamformerMeasure, estimatedOrientations, regularizationParameter, regularizationParameterNoise);
+    }
+    else if(beamformer == Beamformer::SAM) {
+      Scalar regularizationParameterNoise = beamformerConfig.get<Scalar>("regularization_parameter_noise");
+      scalarNAIBeamformer3d(beamformerMeasure, estimatedOrientations, regularizationParameter, regularizationParameterNoise);
+    }
+    else {
+      DUNE_THROW(Dune::Exception, "uknown beamformer");
+    }
+    
+    return {beamformerMeasure, estimatedOrientations};
   }
   
-  std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> getSVD(const Eigen::MatrixXd& matrix)
+  ScanResult vectorSLORETA(const Eigen::VectorXd& topography, const Dune::ParameterTree& sloretaConfig) const
   {
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd;
-    svd.compute(matrix, Eigen::ComputeThinU | Eigen::ComputeThinV);
-    return {svd.matrixU(), svd.singularValues().asDiagonal(), svd.matrixV()};
+    // validate state
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "no leadfield specified");
+    }
+  
+    Eigen::MatrixXd C = sLORETAParameterMatrix(sloretaConfig);
+    
+    std::vector<Scalar> sloretaMeasure(nrSourcePositions_);
+    std::vector<SourceVector> estimatedMoments(nrSourcePositions_);
+    
+    Eigen::VectorXd transformedTopography = C * topography;
+    
+    // iterate over sourcespace
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      Eigen::MatrixXd currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      Eigen::Matrix3d weightMatrix = (currentLeadField.transpose() * C * currentLeadField).inverse();
+      Eigen::Vector3d projectedVector = currentLeadField.transpose() * transformedTopography;
+      sloretaMeasure[i] = (projectedVector.transpose() * weightMatrix * projectedVector).value();
+      estimatedMoments[i] = weightMatrix * projectedVector;
+    }
+    
+    return {sloretaMeasure, estimatedMoments};
+  }
+  
+  std::vector<SourceVector> vectorMNE(const Eigen::VectorXd& topography, const Dune::ParameterTree& mneConfig) const
+  {
+    if(!leadfieldBound_) {
+      DUNE_THROW(Dune::Exception, "no leadfield specified");
+    }
+    
+    Scalar regularizationParameter = mneConfig.get<Scalar>("regularizationParameter");
+    
+    Eigen::MatrixXd gramMatrix = leadfield_ * leadfield_.transpose();
+    gramMatrix += regularizationParameter * Eigen::MatrixXd::Identity(nrChannels_, nrChannels_);
+    
+    Eigen::VectorXd mneReconstruction = leadfield_.transpose() * gramMatrix.inverse() * topography;
+    
+    std::vector<SourceVector> estimatedMoments(nrSourcePositions_);
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      estimatedMoments[i] = mneReconstruction.segment(i * dim, dim);
+    }
+    
+    return estimatedMoments;
   }
   
 private:
 
-  Beamformer beamformerFromString(const std::string& value)
+  // perform a scan based on the UG beamformer power
+  void scalarUGBeamformer3d(std::vector<Scalar>& beamformerMeasure, std::vector<SourceVector>& estimatedOrientations, const Scalar regularizationParameter) const
+  {
+    // compute necessary inverses
+    Eigen::MatrixXd tmpR = signalCovariance_;
+    for(size_t i = 0; i < nrChannels_; ++i) {
+      tmpR(i, i) += regularizationParameter;
+    }
+    Eigen::MatrixXd R_inv = tmpR.inverse();
+    
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      SourceMatrix A = currentLeadField.transpose() * R_inv * currentLeadField;
+      SourceVector orientation = smallestEigenvector(A);
+      
+      beamformerMeasure[i] = 1.0 / (orientation.transpose() * A * orientation).value();
+      estimatedOrientations[i] = orientation;
+    }
+  }
+  
+  // perform a scan based on the AG beamformer power
+  void scalarAGBeamformer3d(std::vector<Scalar>& beamformerMeasure, std::vector<SourceVector>& estimatedOrientations, const Scalar regularizationParameter) const
+  {
+    // compute necessary inverses
+    Eigen::MatrixXd tmpR = signalCovariance_;
+    for(size_t i = 0; i < nrChannels_; ++i) {
+      tmpR(i, i) += regularizationParameter;
+    }
+    Eigen::MatrixXd R_inv = tmpR.inverse();
+    
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      SourceMatrix A = currentLeadField.transpose() * R_inv * currentLeadField;
+      SourceMatrix B = currentLeadField.transpose() * currentLeadField;
+      SourceVector orientation = smallestGeneralizedEigenvector(A, B);
+      
+      beamformerMeasure[i] = (orientation.transpose() * B * orientation).value() / (orientation.transpose() * A * orientation).value();
+      estimatedOrientations[i] = orientation;
+    }
+  }
+  
+  // perform a scan based on the UNG beamformer power
+  void scalarUNGBeamformer3d(std::vector<Scalar>& beamformerMeasure, std::vector<SourceVector>& estimatedOrientations, const Scalar regularizationParameter) const
+  {
+    // compute necessary inverses
+    Eigen::MatrixXd tmpR = signalCovariance_;
+    for(size_t i = 0; i < nrChannels_; ++i) {
+      tmpR(i, i) += regularizationParameter;
+    }
+    Eigen::MatrixXd R_inv = tmpR.inverse();
+    Eigen::MatrixXd R_inv_squared = R_inv * R_inv;
+    
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      SourceMatrix A = currentLeadField.transpose() * R_inv_squared * currentLeadField;
+      SourceMatrix B = currentLeadField.transpose() * R_inv * currentLeadField;
+      SourceVector orientation = smallestGeneralizedEigenvector(A, B);
+      
+      beamformerMeasure[i] = (orientation.transpose() * B * orientation).value() / (orientation.transpose() * A * orientation).value();
+      estimatedOrientations[i] = orientation;
+    }
+  }
+  
+  // perform a scan based on the neural activity index (NAI)
+  void scalarNAIBeamformer3d(std::vector<Scalar>& beamformerMeasure, std::vector<SourceVector>& estimatedOrientations, const Scalar regularizationParameterR, const Scalar regularizationParameterN) const
+  {
+    // compute necessary inverses
+    Eigen::MatrixXd tmpR = signalCovariance_;
+    Eigen::MatrixXd tmpN = noiseCovariance_;
+    for(size_t i = 0; i < nrChannels_; ++i) {
+      tmpR(i, i) += regularizationParameterR;
+      tmpN(i, i) += regularizationParameterN;
+    }
+    Eigen::MatrixXd R_inv = tmpR.inverse();
+    Eigen::MatrixXd N_inv = tmpN.inverse();
+    
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      SourceMatrix A = currentLeadField.transpose() * R_inv * currentLeadField;
+      SourceMatrix B = currentLeadField.transpose() * N_inv * currentLeadField;
+      SourceVector orientation = smallestGeneralizedEigenvector(A, B);
+      
+      beamformerMeasure[i] = (orientation.transpose() * B * orientation).value() / (orientation.transpose() * A * orientation).value();
+      estimatedOrientations[i] = orientation;
+    }
+  }
+  
+  // perform a scan based on the pseudo-Z score (SAM beamformer)
+  void scalarSAMBeamformer3d(std::vector<Scalar>& beamformerMeasure, std::vector<SourceVector>& estimatedOrientations, const Scalar regularizationParameterR, const Scalar regularizationParameterN) const
+  {
+    // compute necessary inverses
+    Eigen::MatrixXd tmpR = signalCovariance_;
+    Eigen::MatrixXd tmpN = noiseCovariance_;
+    for(size_t i = 0; i < nrChannels_; ++i) {
+      tmpR(i, i) += regularizationParameterR;
+      tmpN(i, i) += regularizationParameterN;
+    }
+    Eigen::MatrixXd R_inv = tmpR.inverse();
+    Eigen::MatrixXd N_conj = R_inv * tmpN * R_inv;
+    
+    for(size_t i = 0; i < nrSourcePositions_; ++i) {
+      LeadfieldMatrix currentLeadField = leadfield_.block(0, dim * i, nrChannels_, dim);
+      SourceMatrix A = currentLeadField.transpose() * N_conj * currentLeadField;
+      SourceMatrix B = currentLeadField.transpose() * R_inv * currentLeadField;
+      SourceVector orientation = smallestGeneralizedEigenvector(A, B);
+      
+      beamformerMeasure[i] = (orientation.transpose() * B * orientation).value() / (orientation.transpose() * A * orientation).value();
+      estimatedOrientations[i] = orientation;
+    }
+  }
+
+  Beamformer beamformerFromString(const std::string& value) const
   {
     // make string uppercase to cover different spellings
     std::string valueLocal(value);
@@ -151,6 +355,73 @@ private:
       return Beamformer::SAM;
     else
       DUNE_THROW(Dune::Exception, "unknown beamformer, please choose one from {UG, AG, UNG, NAI, SAM}");
+  }
+
+  // In the paper here
+  // https://arxiv.org/pdf/0710.3341.pdf
+  // Pascual-Marqui defines a family of what he calls "linear imaging methods", which correctly localize single point dipoles
+  // when applied to noiseless samples. This family is parametrized by a positive semidefinite matrix C, which has to fullfill certain conditions.
+  // We here implement two choices for C, first the choice leading to the classical sLORETA method, and second the version due to Sekihara in his book
+  // "Adaptive Spatial Filters for Electromagnetic Brain Imaging". In exact arithmetic, these two approaches are exactly equivalent, and I expect
+  // they will also produce similar results in simulations.
+  Eigen::MatrixXd sLORETAParameterMatrix(const Dune::ParameterTree& config) const
+  {
+    std::string parameterMatrixString = config.get<std::string>("parameterMatrix", "sekihara");
+    Scalar regularizationParameter = config.get<Scalar>("regularization_parameter");
+    for(auto& character : parameterMatrixString) {
+      character = std::toupper(character);
+    }
+    
+    Eigen::MatrixXd gramMatrix = leadfield_ * leadfield_.transpose();
+    Eigen::MatrixXd parameterMatrix;
+    
+    if(parameterMatrixString == "ORIGINAL") {
+      // compute orthogonal projection onto orthogonal complement of constant vector
+      Eigen::MatrixXd projectionOntoConstant = Eigen::MatrixXd::Constant(nrChannels_, 1, 1); 
+      Eigen::MatrixXd projectionOntoComplement = Eigen::MatrixXd::Identity(nrChannels_, nrChannels_) - projectionOntoConstant * projectionOntoConstant.transpose() / (projectionOntoConstant.transpose() * projectionOntoConstant).value();
+      
+      gramMatrix += regularizationParameter * projectionOntoComplement;
+      
+      Eigen::CompleteOrthogonalDecomposition<Eigen::MatrixXd> cod(nrChannels_, nrChannels_);
+      cod.setThreshold(10.0 * nrChannels_ * nrSourcePositions_ * std::numeric_limits<Scalar>::epsilon());
+      cod.compute(gramMatrix);
+      parameterMatrix = cod.pseudoInverse();
+    }
+    else if(parameterMatrixString == "SEKIHARA") {
+      gramMatrix += regularizationParameter * Eigen::MatrixXd::Identity(nrChannels_, nrChannels_);
+      parameterMatrix = gramMatrix.inverse();
+    }
+    else {
+      DUNE_THROW(Dune::Exception, "unknown parameter matrix string " << parameterMatrixString);
+    }
+    
+    return parameterMatrix;
+  }
+
+  // compute the eigenvector solving A x = lambda x, where lambda is minimal
+  // This function assumes that A is positive definite
+  SourceVector smallestEigenvector(const SourceMatrix& A) const
+  {
+    Eigen::SelfAdjointEigenSolver<SourceMatrix> eigenSolver(A);
+    SourceVector eigenvector = eigenSolver.eigenvectors().col(0);
+    return eigenvector;
+  }
+
+  // Compute a generalized eigenvector solving A * x = lambda * B * x, where lambda is minimal
+  // This function assumes that A and B are symmetric positive definite 
+  SourceVector smallestGeneralizedEigenvector(const SourceMatrix& A, const SourceMatrix& B) const
+  {
+    Eigen::GeneralizedSelfAdjointEigenSolver<SourceMatrix> generalizedEigenSolver(A, B);
+    SourceVector eigenvector = generalizedEigenSolver.eigenvectors().col(0);
+    eigenvector /= eigenvector.norm();
+    return eigenvector;
+  }
+  
+  std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> getSVD(const Eigen::MatrixXd& matrix)
+  {
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd;
+    svd.compute(matrix, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    return {svd.matrixU(), svd.singularValues().asDiagonal(), svd.matrixV()};
   }
 
   Dune::ParameterTree config_;
