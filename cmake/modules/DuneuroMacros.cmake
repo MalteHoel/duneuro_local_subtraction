62794d13a673312a8fd348e2f14a57558ed79307
Malte <m_hoel20@uni-muenster.de>
2024-09-23 17:34:25 +0200

    remove unused cmake code for TBB

--- cmake/modules/DuneuroMacros.cmake
+++ cmake/modules/DuneuroMacros.cmake
@@ -5,16 +5,6 @@ find_package(HDF5WRAP)
 find_package(Nifti)
 find_package(TBB)
 
-# set(HAVE_TBB ${TBB_FOUND})
-# if (TBB_FOUND)
-#    link_libraries{PUBLIC TBB::tbb)
-#    compile_definitions(${_target} PUBLIC ENABLE_TBB=1)
-#  dune_register_package_flags(COMPILE_DEFINITIONS "ENABLE_TBB=1"
-#    INCLUDE_DIRS ${TBB_INCLUDE_DIRS}
-#    LIBRARIES ${TBB_LIBRARIES}
-#    COMPILE_DEFINITIONS ${TBB_DEFINITIONS})
-# endif ()
-
 include(AddTBBFlags)
 include(AddHDF5Flags)
 include(AddNiftiFlags)
