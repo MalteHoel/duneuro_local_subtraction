343a4b39c34256b0edd7afcbfff52589e49db2af
Malte <m_hoel20@uni-muenster.de>
2024-12-27 14:59:58 +0100

    add Matlab to CI pipeline

---
+++ ci_information/resolve_non_dune_dependencies.sh
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Copyright © duneuro contributors, see file LICENSE.md in module root
+# SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-duneuro-exception OR LGPL-3.0-or-later
+apt update
+apt upgrade -y
+apt install -y \
+  build-essential \
+  cmake \
+  git \
+  libeigen3-dev \
+  libpython3-dev \
+  libsuitesparse-dev \
+  libtbb-dev \
+  libsuperlu-dev
